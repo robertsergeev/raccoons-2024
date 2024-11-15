92d13421a18a95a07a04f8f923ad25308c7c0f68
kOSD30 <dominikskorohodov@gmail.com>
2024-11-15 21:20:26 +0200

    New version for raccon2024

--- src/components/TransactionItem/TransactionItem.jsx
+++ src/components/TransactionItem/TransactionItem.jsx
@@ -1,14 +1,23 @@
-import "./TransactionItem.css"
+import React from 'react';
+import './TransactionItem.css';
 
-const TransactionItem = ({transaction}) => {
+const TransactionItem = ({ transaction }) => {
+    const { name, method, date, amount } = transaction;
+    const amountClass = amount >= 0 ? "amount-cell positive" : "amount-cell negative";
+
     return (
-        <div className="transaction">
-            <h2>{transaction.name}</h2>
-            <p>{transaction.amount}</p>
-            <p>{transaction.category}</p>
-            <p>{transaction.date}</p>
-    </div>
-    )
-}
+        <tr>
+            <td className="description-cell">
+                <div className="icon">{name.charAt(0)}</div>
+                <span>{name}</span>
+            </td>
+            <td className="method-cell">{method}</td>
+            <td className="date-cell">{date}</td>
+            <td className={amountClass}>
+                {amount >= 0 ? `+ $${amount.toFixed(2)}` : `- $${Math.abs(amount).toFixed(2)}`}
+            </td>
+        </tr>
+    );
+};
 
-export default TransactionItem
+export default TransactionItem;
